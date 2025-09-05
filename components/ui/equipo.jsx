b7a58df4f14dd600f2b670b0bd46b8b74cade451
Emkauno <emkauno@gmail.com>
2025-09-04 20:08:56 -0400

    update: contenido claudio

--- components/ui/equipo.jsx
+++ components/ui/equipo.jsx
@@ -18,7 +18,7 @@ export function EquipoCarousel() {
     },
     {
       quote:
-        "Trabajador Social de la Universidad Tecnológica Metropolitana (UTEM). Acompaño a familias con integrantes con necesidades psicosociales, cognitivas y neurodivergentes. Me intereso en contextualizar el proceso histórico en que desarrollan su ciclo vital. Todo mi acompañamiento lo basa en el respeto a todo evento de la dignidad del ser humano por sobre toda creencia e interés personal, valorando la importancia de la familia y el vínculo con su entorno.",
+        "Trabajador Social de la Universidad Tecnológica Metropolitana (UTEM) y Gerontólogo Psicosocial. Acompaño a familias y personas mayores con necesidades psicosociales, cognitivas y neurodivergentes. Me intereso en contextualizar el proceso histórico en que desarrollan su ciclo vital. Todo mi acompañamiento se basa en el respeto a todo evento de la dignidad del ser humano por sobre toda creencia e interés personal, valorando la importancia de la familia y el vínculo con su entorno.",
       name: "CLAUDIO SOTO CASTRO",
       designation: "Gestión General",
       src: "/claudio.jpg",
