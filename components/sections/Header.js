c5acdc90dc2125694b168a5678f7d6e82fd4af66
Emkauno <emkauno@gmail.com>
2025-11-04 21:48:16 -0300

    fix menu

--- components/sections/Header.js
+++ components/sections/Header.js
@@ -23,6 +23,9 @@ const HeaderWrapper = styled.header`
     justify-content: center;
     align-items: center; 
   }
+  @media(max-width: 768px){
+    padding: 1rem;
+  }
 `;
 
 const Logo = styled.img`
@@ -81,38 +84,86 @@ const Hamburger = styled.div`
   gap: 6px;
   cursor: pointer;
   position: absolute; 
-  top: 30px;
-  right: 30px;
+  right: 1rem;
   z-index: 60;
+  width: 40px;
+  height: 40px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  -webkit-tap-highlight-color: transparent;
+
+  .ham {
+    transform: scale(0.4);
+  }
+  svg {
+  transition: transform 500ms cubic-bezier(0.4, 0, 0.2, 1);
+}
+.active svg {
+  transform: rotate(90deg);
+}
+path {
+  transition: transform 500ms cubic-bezier(0.4, 0, 0.2, 1),
+    stroke-dasharray 500ms cubic-bezier(0.4, 0, 0.2, 1),
+    stroke-dashoffset 500ms cubic-bezier(0.4, 0, 0.2, 1);
+}
+path:nth-child(1) {
+  transform-origin: 36% 40%;
+}
+path:nth-child(2) {
+  stroke-dasharray: 29 299;
+}
+path:nth-child(3) {
+  transform-origin: 35% 63%;
+}
+path:nth-child(4) {
+  stroke-dasharray: 29 299;
+}
+path:nth-child(5) {
+  transform-origin: 61% 52%;
+}
+path:nth-child(6) {
+  transform-origin: 62% 52%;
+}
+.active path:nth-child(1) {
+  transform: translateX(9px) translateY(1px) rotate(45deg);
+}
+.active path:nth-child(2) {
+  stroke-dasharray: 225 299;
+  stroke-dashoffset: -72px;
+}
+.active path:nth-child(3) {
+  transform: translateX(9px) translateY(1px) rotate(-45deg);
+}
+.active path:nth-child(4) {
+  stroke-dasharray: 225 299;
+  stroke-dashoffset: -72px;
+}
+.active path:nth-child(5) {
+  transform: translateX(9px) translateY(1px) rotate(-45deg);
+}
+.active path:nth-child(6) {
+  transform: translateX(9px) translateY(1px) rotate(45deg);
+}
 
   @media (min-width: 768px) {
     display: none;
   }
-
-  div {
-    height: 2px;
-    width: 24px;
-    background: #111;
-    transition: all 0.3s ease;
-  z-index: 60;
-
-
     .dark & {
       background: #eee;
     }
-  }
 `;
 
 const MobileMenuWrapper = styled(motion.div)`
   position: fixed;
   inset: 0;
-  z-index: 55;
+  z-index: 45;
   color: black;
   display: flex;
   flex-direction: column;
   justify-content: center;
   align-items: center;
-
+  background: white;
   .dark & {
     background: #111;
     color: white;
@@ -166,9 +217,46 @@ export const Header = () => {
         </Nav>
 
         <Hamburger onClick={() => setOpen(!open)}>
-          <motion.div animate={{ rotate: open ? 45 : 0, y: open ? 6 : 0 }} />
-          <motion.div animate={{ opacity: open ? 0 : 1 }} />
-          <motion.div animate={{ rotate: open ? -45 : 0, y: open ? -6 : 0 }} />
+          <div className={open ? 'ham active': 'ham'}>
+  <svg xmlns="http://www.w3.org/2000/svg" width="150" height="150" viewBox="0 0 200 200">
+        <g stroke-width="6.5" stroke-linecap="round">
+          <path
+            d="M72 82.286h28.75"
+            fill="#009100"
+            fill-rule="evenodd"
+            stroke="#000"
+          />
+          <path
+            d="M100.75 103.714l72.482-.143c.043 39.398-32.284 71.434-72.16 71.434-39.878 0-72.204-32.036-72.204-71.554"
+            fill="none"
+            stroke="#000"
+          />
+          <path
+            d="M72 125.143h28.75"
+            fill="#009100"
+            fill-rule="evenodd"
+            stroke="#000"
+          />
+          <path
+            d="M100.75 103.714l-71.908-.143c.026-39.638 32.352-71.674 72.23-71.674 39.876 0 72.203 32.036 72.203 71.554"
+            fill="none"
+            stroke="#000"
+          />
+          <path
+            d="M100.75 82.286h28.75"
+            fill="#009100"
+            fill-rule="evenodd"
+            stroke="#000"
+          />
+          <path
+            d="M100.75 125.143h28.75"
+            fill="#009100"
+            fill-rule="evenodd"
+            stroke="#000"
+          />
+        </g>
+      </svg>
+</div>
         </Hamburger>
       </HeaderWrapper>
 
